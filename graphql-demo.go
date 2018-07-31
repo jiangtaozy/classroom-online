0ab5b8439d9804902eb59460883531a81fe85e95
jemo <1321332562@qq.com>
2018-08-01 07:39:06 +0800

    add graphql demo

---
+++ graphql-demo.go
@@ -0,0 +1,54 @@
+/*
+ * Created by jemo on 2018-8-1.
+ * graphql demo
+ */
+
+package main
+
+import (
+  "encoding/json"
+  "fmt"
+  "log"
+  "github.com/graphql-go/graphql"
+)
+
+func main() {
+  // Schema
+  fields := graphql.Fields{
+    "hello": &graphql.Field{
+      Type: graphql.String,
+      Resolve: func(p graphql.ResolveParams) (interface{},error) {
+        return "world", nil
+      },
+    },
+  }
+  rootQuery := graphql.ObjectConfig{
+    Name: "RootQuery",
+    Fields: fields,
+  }
+  schemaConfig := graphql.SchemaConfig{
+    Query: graphql.NewObject(rootQuery),
+  }
+  schema, err := graphql.NewSchema(schemaConfig)
+  if err != nil {
+    log.Fatalf("failed to create new schema, error: %v", err)
+  }
+
+  // Query
+  query := `
+    {
+      hello
+    }
+  `
+  params := graphql.Params{
+    Schema: schema,
+    RequestString: query,
+  }
+  r := graphql.Do(params)
+  if len(r.Errors) > 0 {
+    log.Fatalf("failed to execute graphql opration, errors: %+v",
+      r.Errors)
+  }
+  rJSON, _ := json.Marshal(r)
+  fmt.Printf("%s \n", rJSON)
+}
