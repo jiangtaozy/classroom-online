ab37c360c9121da2dcb5a8d8bda01715b726dd34
jemo <1321332562@qq.com>
2019-03-24 09:34:54 +0800

    format signal

--- signal/hub.go
+++ signal/hub.go
@@ -1,9 +1,6 @@
 /*
  * Maintained by jemo from 2018.8.6 to now
  * Created by jemo on 2018.8.6 22:35:47
- * Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
- * Use of this source code is governed by a BSD-style
- * license that can be found in the LICENSE file.
  */
 
 package signal
@@ -22,70 +19,67 @@ type Message struct {
 // hub maintains the set of active clients and broadcasts messages to the
 // clients.
 type Hub struct {
-	// Registered clients.
-	clients map[*Client]bool
-
-	// Inbound messages from the clients.
-	// broadcast chan []byte
-	broadcast chan *Message
-
-	// Register requests from the clients.
-	register chan *Client
-
-	// Unregister requests from clients.
-	unregister chan *Client
+  // Registered clients.
+  clients map[*Client]bool
+  // Inbound messages from the clients.
+  // broadcast chan []byte
+  broadcast chan *Message
+  // Register requests from the clients.
+  register chan *Client
+  // Unregister requests from clients.
+  unregister chan *Client
 }
 
 func NewHub() *Hub {
-	return &Hub{
-		// broadcast:  make(chan []byte),
-		broadcast:  make(chan *Message),
-		register:   make(chan *Client),
-		unregister: make(chan *Client),
-		clients:    make(map[*Client]bool),
-	}
+  return &Hub{
+    // broadcast:  make(chan []byte),
+    broadcast:  make(chan *Message),
+    register:   make(chan *Client),
+    unregister: make(chan *Client),
+    clients:    make(map[*Client]bool),
+  }
 }
 
 func (h *Hub) Run() {
-	for {
-                if len(h.clients) > 1 {
-			select {
-			case client := <-h.register:
-                                log.Printf("register\n")
-				h.clients[client] = true
-			case client := <-h.unregister:
-				if _, ok := h.clients[client]; ok {
-                                        log.Printf("unregister\n")
-					delete(h.clients, client)
-					close(client.send)
-				}
-			case message := <-h.broadcast:
-				for client := range h.clients {
-                                        if message.client != client {
-                                                //log.Printf("message.client != client")
-                                                log.Printf("message.msg: %s\n", message.msg)
-						select {
-						//case client.send <- message:
-						case client.send <- message.msg:
-						default:
-							close(client.send)
-							delete(h.clients, client)
-						}
-                                        } else {
-                                                //log.Printf("message.client == client")
-                                        }
-				}
-			}
-		} else {
-			select {
-			case client := <-h.register:
-				h.clients[client] = true
-			case client := <-h.unregister:
-				if _, ok := h.clients[client]; ok {
-					delete(h.clients, client)
-					close(client.send)
-				}
-			}
-		}
-	}
+  for {
+    if len(h.clients) > 1 {
+      select {
+      case client := <-h.register:
+        log.Printf("register\n")
+        h.clients[client] = true
+      case client := <-h.unregister:
+        if _, ok := h.clients[client]; ok {
+          log.Printf("unregister\n")
+          delete(h.clients, client)
+          close(client.send)
+        }
+      case message := <-h.broadcast:
+        for client := range h.clients {
+          if message.client != client {
+            //log.Printf("message.client != client")
+            log.Printf("message.msg: %s\n", message.msg)
+            select {
+              //case client.send <- message:
+            case client.send <- message.msg:
+            default:
+              close(client.send)
+              delete(h.clients, client)
+            }
+          } else {
+            //log.Printf("message.client == client")
+          }
+        }
+      }
+    } else {
+      select {
+      case client := <-h.register:
+        h.clients[client] = true
+      case client := <-h.unregister:
+        if _, ok := h.clients[client]; ok {
+          delete(h.clients, client)
+          close(client.send)
+        }
+      }
+    }
+  }
 }
