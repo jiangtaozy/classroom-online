0444c7ea4e8d02e90d043408843f2483dd42624c
jemo <1321332562@qq.com>
2019-04-06 09:48:30 +0800

    teacher list load more

--- client/src/components/school/index.js
+++ client/src/components/school/index.js
@@ -5,17 +5,49 @@
  */
 
 import React, { Component } from 'react'
-import { graphql, createFragmentContainer } from 'react-relay'
+import { graphql, createPaginationContainer } from 'react-relay'
 import Typography from '@material-ui/core/Typography'
 import { Link } from 'react-router-dom'
 
+
 class School extends Component {
+
+  componentDidMount() {
+    window.onscroll = () => {
+      const {
+        innerHeight,
+        scrollY
+      } = window
+      if ((innerHeight + scrollY) >= document.body.offsetHeight) {
+        this.loadMore()
+      }
+    }
+  }
+
+  loadMore = () => {
+    const {
+      hasMore,
+      isLoading,
+      loadMore,
+    } = this.props.relay
+    if(!hasMore() || isLoading()) {
+      return
+    }
+    loadMore(
+      2,
+      error => {
+        if(error) {
+          console.error('schoolLoadMoreError: ', error)
+        }
+      }
+    )
+  }
+
   render() {
     const { viewer } = this.props
     const { userList } = viewer || {}
     const {
       edges,
-      //pageInfo,
     } = userList || {}
     return (
       <div>
@@ -94,22 +126,58 @@ class School extends Component {
   }
 }
 
-export default createFragmentContainer(School, {
-  viewer: graphql`
-    fragment school_viewer on Viewer {
-      userList(
-        first: 2147483647
-      ) @connection(key: "school_userList") {
-        edges {
-          node {
-            id,
-            avatar,
-            nickname,
-            introduction,
-            backgroundImage,
+export default createPaginationContainer(
+  School,
+  {
+    viewer: graphql`
+      fragment school_viewer on Viewer
+      @argumentDefinitions(
+        count: {type: "Int", defaultValue: 2}
+        cursor: {type: "String"}
+      ) {
+        userList(
+          first: $count
+          after: $cursor
+        ) @connection(key: "school_userList") {
+          edges {
+            node {
+              id,
+              avatar,
+              nickname,
+              introduction,
+              backgroundImage,
+            }
           }
         }
       }
-    }
-  `,
-})
+    `,
+  },
+  {
+    direction: 'forward',
+    getConnectionFromProps(props) {
+      return props.viewer && props.viewer.userList
+    },
+    getFragmentVariables(prevVars, totalCount) {
+      return {
+        ...prevVars,
+        count: totalCount,
+      }
+    },
+    getVariables(props, {count, cursor}, fragmentVariables) {
+      return {
+        count,
+        cursor,
+      }
+    },
+    query: graphql`
+      query schoolPaginationQuery(
+        $count: Int!
+        $cursor: String
+      ) {
+        viewer {
+          ...school_viewer @arguments(count: $count, cursor: $cursor)
+        }
+      }
+    `
+  }
+)
